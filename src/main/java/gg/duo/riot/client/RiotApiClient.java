d23dff0689044f0125aae280b5a795fd369bcffa
HwantaLee <ltw0527@naver.com>
2026-07-22 11:31:15 +0900

    feat: implement Riot Account API client

--- src/main/java/gg/duo/riot/client/RiotApiClient.java
+++ src/main/java/gg/duo/riot/client/RiotApiClient.java
@@ -1,4 +1,29 @@
 package gg.duo.riot.client;
 
+import gg.duo.riot.dto.response.AccountResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.client.WebClient;
+
+@Component
+@RequiredArgsConstructor
 public class RiotApiClient {
+
+    private final WebClient riotWebClient;
+
+    @Value("${riot.api.key}")
+    private String apiKey;
+
+    public AccountResponse getAccount(String gameName, String tagLine) {
+
+        return riotWebClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/riot/account/v1/accounts/by-riot-id/{gameName}/{tagLine}")
+                        .build(gameName, tagLine))
+                .header("X-Riot-Token", apiKey)
+                .retrieve()
+                .bodyToMono(AccountResponse.class)
+                .block();
+    }
 }
--- src/main/java/gg/duo/riot/dto/response/AccountResponse.java
+++ src/main/java/gg/duo/riot/dto/response/AccountResponse.java
@@ -1,4 +1,15 @@
 package gg.duo.riot.dto.response;
 
-public class AccountResponseDTO {
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+public class AccountResponse {
+
+    private String puuid;
+
+    private String gameName;
+
+    private String tagLine;
 }
