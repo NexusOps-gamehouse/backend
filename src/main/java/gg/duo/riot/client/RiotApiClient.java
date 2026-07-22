72a2d09781904978249163f77f853f2a0a3e8a01
HwantaLee <ltw0527@naver.com>
2026-07-22 16:48:19 +0900

    feat : add summonerAPI logic at RiotApiClient

--- src/main/java/gg/duo/riot/client/RiotApiClient.java
+++ src/main/java/gg/duo/riot/client/RiotApiClient.java
@@ -1,6 +1,7 @@
 package gg.duo.riot.client;
 
 import gg.duo.riot.dto.response.AccountResponseDTO;
+import gg.duo.riot.dto.response.SummonerResponseDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -24,6 +25,18 @@ public class RiotApiClient {
                 .header("X-Riot-Token", apiKey)
                 .retrieve()
                 .bodyToMono(AccountResponseDTO.class)
+                .block();
+    }
+
+    public SummonerResponseDTO getSummoner(String puuid) {
+
+        return riotWebClient.get()
+                .uri(uriBuilder -> uriBuilder
+                        .path("/lol/summoner/v4/summoners/by-puuid/{puuid}")
+                        .build(puuid))
+                .header("X-Riot-Token", apiKey)
+                .retrieve()
+                .bodyToMono(SummonerResponseDTO.class)
                 .block();
     }
 }
