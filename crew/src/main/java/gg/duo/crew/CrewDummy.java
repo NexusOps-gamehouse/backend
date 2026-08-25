876b00229d0008f4bca311cf1bf643f6c0aeb300
lorancherry-wq <lorancherry@gmail.com>
2026-08-25 12:27:49 +0900

    fix: crew 모듈 resources 경로 추가 및 실행 설정 수정

--- crew/src/main/java/gg/duo/crew/CrewDummy.java
+++ crew/src/main/java/gg/duo/crew/CrewDummy.java
@@ -1,4 +1,11 @@
 package gg.duo.crew;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class CrewDummy {
+    public static void main(String[] args) {
+        SpringApplication.run(CrewDummy.class, args);
+    }
 }
